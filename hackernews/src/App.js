75f7a9aa4510424b3bac88d5a0cb52e4069012ee
Andre Pacheco <andre@COMP81.local>
2018-04-23 15:43:53 -0600

    Converted the input element into a controlled component by setting the value.

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -78,6 +78,7 @@ class App extends Component {
         <form>
           <input
             type="text"
+            value={searchTerm}
             onChange={this.onSearchChange}
           />
         </form>
