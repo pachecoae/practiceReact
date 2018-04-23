fc4200542054b2ba9b5e87b04656894033d90863
Andre Pacheco <andre@COMP81.local>
2018-04-23 15:37:40 -0600

    Learned about destructuring.

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -37,6 +37,15 @@ const list = [
   },
 ]
 
+const isSearched = searchTerm => item =>
+  item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
+// function isSearched(searchTerm) {
+//   return function(item) {
+//     return item.title.toLowerCase().includes(searchTerm.toLowerCase());
+//   }
+// }
+
 class App extends Component {
 
   constructor(props) {
@@ -44,6 +53,7 @@ class App extends Component {
 
     this.state = {
       list,
+      searchTerm: '',
     }
 
   }
@@ -54,11 +64,24 @@ class App extends Component {
     this.setState({ list: updatedList })
   }
 
+  onSearchChange = (event) => {
+    this.setState({ searchTerm: event.target.value })
+  }
+
   render() {
+    const {
+      searchTerm,
+      list
+    } = this.state;
     return (
       <div className="App">
-        {this.state.list.map(item => {
-          const onHandleDismiss = () => this.onDismiss(item.objectID);
+        <form>
+          <input
+            type="text"
+            onChange={this.onSearchChange}
+          />
+        </form>
+        {list.filter(isSearched(searchTerm)).map(item =>
           <div key={item.objectID}>
             <span>
               <a href={item.url}>{item.title}</a>
@@ -72,14 +95,14 @@ class App extends Component {
             <span> </span>
             <span>
               <button
-                onClick={onHandleDismiss}
+                onClick={() => this.onDismiss(item.objectID)}
                 type="button"
               >
                 Dismiss
               </button>
             </span>
           </div>
-        })}
+        )}
       </div>
     );
   }
