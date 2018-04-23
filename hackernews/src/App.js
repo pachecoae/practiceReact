882476889a2e278d915577502c4940f8d90bf422
Andre Pacheco <andre@COMP81.local>
2018-04-23 14:22:04 -0600

    Learned how to bind a method to a class without explicitly binding it in the constructor. Added a button to remove objects in a list to demonstrate unidirectional data flow.

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -7,8 +7,8 @@ const list = [
     title: 'React',
     url:'https://facebook.github.io/react/',
     author: 'Person One',
-    num_comments: 3,
-    points: 4,
+    num_comments: 1,
+    points: 1,
     objectID: 0,
   },
   {
@@ -16,7 +16,7 @@ const list = [
     url:'https://github.com/reactjs/redux',
     author: 'Person Two',
     num_comments: 2,
-    points: 5,
+    points: 2,
     objectID: 1,
   },
   {
@@ -24,15 +24,15 @@ const list = [
     url:'https://facebook.github.io/react/',
     author: 'Person Three',
     num_comments: 3,
-    points: 4,
+    points: 3,
     objectID: 2,
   },
   {
     title: 'Redux',
     url:'https://github.com/reactjs/redux',
     author: 'Person Four',
-    num_comments: 2,
-    points: 5,
+    num_comments: 4,
+    points: 4,
     objectID: 3,
   },
 ]
@@ -45,6 +45,13 @@ class App extends Component {
     this.state = {
       list,
     }
+
+  }
+
+  onDismiss = (id) => {
+    const isNotId = item => item.objectID !== id;
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({ list: updatedList })
   }
 
   render() {
@@ -61,6 +68,15 @@ class App extends Component {
               <span>{item.num_comments}</span>
               <span> </span>
               <span>{item.points}</span>
+              <span> </span>
+              <span>
+                <button
+                  onClick={() => this.onDismiss(item.objectID)}
+                  type="button"
+                >
+                  Dismiss
+                </button>
+              </span>
             </div>
         )}
       </div>
