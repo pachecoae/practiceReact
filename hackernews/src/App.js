d676ea216766ddafadfd18493ed5fb532616281d
Andre Pacheco <andre@COMP81.local>
2018-04-24 12:00:59 -0600

    Learned about component declarations and functional stateless components.

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -97,92 +97,45 @@ class App extends Component {
 
 }
 
-class Search extends Component {
+const Search = ({value, onChange, children}) =>
+  <form>
+    {children} <input
+      type="text"
+      value={value}
+      onChange={onChange}
+    />
+  </form>
 
-  render() {
-
-    const {
-      value,
-      onChange,
-      children,
-    } = this.props;
-
-    return (
-      <form>
-        {children} <input
-          type="text"
-          value={value}
-          onChange={onChange}
-        />
-      </form>
-    );
-
-  }
-
-}
-
-class Table extends Component {
-
-  render() {
-
-    const {
-      list,
-      pattern,
-      onDismiss,
-    } = this.props;  
-
-    return (
-      <div>
-        {list.filter(isSearched(pattern)).map(item =>
-          <div key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span> </span>
-            <span>{item.author}</span>
-            <span> </span>
-            <span>{item.num_comments}</span>
-            <span> </span>
-            <span>{item.points}</span>
-            <span> </span>
-            <span>
-              <Button onClick={() => onDismiss(item.objectID)}>
-                Dismiss
-              </Button>
-            </span>
-          </div>
-        )}
+const Table = ({list, pattern, onDismiss}) => 
+  <div>
+    {list.filter(isSearched(pattern)).map(item =>
+      <div key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span> </span>
+        <span>{item.author}</span>
+        <span> </span>
+        <span>{item.num_comments}</span>
+        <span> </span>
+        <span>{item.points}</span>
+        <span> </span>
+        <span>
+          <Button onClick={() => onDismiss(item.objectID)}>
+            Dismiss
+          </Button>
+        </span>
       </div>
-    );
+    )}
+  </div>
 
-  }
-
-}
-
-class Button extends Component {
-
-  render() {
-
-    const {
-      onClick,
-      className = '',
-      children,
-    } = this.props;
-    
-    return (
-
-      <button
-        onClick={onClick}
-        className={className}
-        type="button"
-      >
-        {children}
-      </button>
-
-    );
-
-  }
-
-}
+const Button = ({onClick, className = '', children}) => 
+  <button
+    onClick={onClick}
+    className={className}
+    type="button"
+  >
+    {children}
+  </button>
 
 export default App;
