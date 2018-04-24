a2c18aeaf0a28a8ca4fdab05a186eccb98ce1595
Andre Pacheco <andre@COMP81.local>
2018-04-24 10:10:52 -0600

    Broke up app into separate components.

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -69,20 +69,68 @@ class App extends Component {
   }
 
   render() {
+
     const {
       searchTerm,
       list
     } = this.state;
+
     return (
+
       <div className="App">
-        <form>
-          <input
-            type="text"
-            value={searchTerm}
-            onChange={this.onSearchChange}
-          />
-        </form>
-        {list.filter(isSearched(searchTerm)).map(item =>
+        <Search
+          value={searchTerm}
+          onChange={this.onSearchChange}
+        />
+        <Table
+          list={list}
+          pattern={searchTerm}
+          onDismiss={this.onDismiss}
+        />
+      </div>
+
+    );
+
+  }
+
+}
+
+class Search extends Component {
+
+  render() {
+
+    const {
+      value,
+      onChange
+    } = this.props;
+
+    return (
+      <form>
+        <input
+          type="text"
+          value={value}
+          onChange={onChange}
+        />
+      </form>
+    );
+
+  }
+
+}
+
+class Table extends Component {
+
+  render() {
+
+    const {
+      list,
+      pattern,
+      onDismiss
+    } = this.props;  
+
+    return (
+      <div>
+        {list.filter(isSearched(pattern)).map(item =>
           <div key={item.objectID}>
             <span>
               <a href={item.url}>{item.title}</a>
@@ -96,7 +144,7 @@ class App extends Component {
             <span> </span>
             <span>
               <button
-                onClick={() => this.onDismiss(item.objectID)}
+                onClick={() => onDismiss(item.objectID)}
                 type="button"
               >
                 Dismiss
@@ -106,6 +154,7 @@ class App extends Component {
         )}
       </div>
     );
+
   }
 
 }
